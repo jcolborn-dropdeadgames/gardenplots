cf95cd006ddd6304a831c897adaf61a20b3de4ee
Jacob Colborn <jake@jakesweb.me>
2018-03-05 23:51:28 -0500

    setup test express server and webpage

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '/../static')))
 
 app.listen(3000, () => console.log('Example app listening on port 3000'));
